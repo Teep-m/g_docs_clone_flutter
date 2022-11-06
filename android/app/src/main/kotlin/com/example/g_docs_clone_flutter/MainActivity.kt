19af57d8ee28f26789afa57d37430061d9bc0cd0
Teep-m <103185983+Teep-m@users.noreply.github.com>
2022-11-06 19:43:16 +0900

    initial commit

---
+++ android/app/src/main/kotlin/com/example/g_docs_clone_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.g_docs_clone_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton.icon(
+            onPressed: () {},
+            icon: Image.asset('assets/image/g-logo-2.png'),
+            label: const Text('Sign in with Google')),
+      ),
+    );
+  }
+}
