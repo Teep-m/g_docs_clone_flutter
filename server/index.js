e43576e53ab575659eef6156a47b9a8a7fc235d5
Teep-m <103185983+Teep-m@users.noreply.github.com>
2022-11-09 20:08:15 +0900

    creating server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,22 @@
-//import 'package:http://http.dart'
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT | 3001;
 
 const app = express();
 
-app.listen();
+const DB = "mongodb+srv://tetsu:13silvia@cluster0.1x5pyca.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection successful");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`connected at port ${PORT}`);
+  console.log("hey this is changing automatically");
+});
