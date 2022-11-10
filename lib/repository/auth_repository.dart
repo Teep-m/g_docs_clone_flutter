9eec25e29a16d1b65c645003ef7fc1f078f21080
Teep-m <103185983+Teep-m@users.noreply.github.com>
2022-11-10 23:39:25 +0900

    adding auth middleware

--- lib/repository/auth_repository.dart
+++ lib/repository/auth_repository.dart
@@ -4,6 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:g_docs_clone_flutter/constants.dart';
 import 'package:g_docs_clone_flutter/models/error_model.dart';
 import 'package:g_docs_clone_flutter/models/user_model.dart';
+import 'package:g_docs_clone_flutter/repository/local_storage_repo.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:http/http.dart';
 
@@ -11,6 +12,7 @@ final authRepositoryProvider = Provider(
   (ref) => AuthRepository(
     googleSignIn: GoogleSignIn(),
     client: Client(),
+    localStorageRepo: LocalStorageRepo(),
   ),
 );
 
@@ -19,9 +21,14 @@ final userProvider = StateProvider<UserModel?>((ref) => null);
 class AuthRepository {
   final GoogleSignIn _googleSignIn;
   final Client _client;
-  AuthRepository({required GoogleSignIn googleSignIn, required Client client})
-      : _googleSignIn = googleSignIn,
-        _client = client;
+  final LocalStorageRepo _localStorageRepo;
+  AuthRepository({
+    required GoogleSignIn googleSignIn,
+    required Client client,
+    required LocalStorageRepo localStorageRepo,
+  })  : _googleSignIn = googleSignIn,
+        _client = client,
+        _localStorageRepo = localStorageRepo;
 
   Future<ErrorModel> signInWithGoogle() async {
     ErrorModel error = ErrorModel(
@@ -52,6 +59,40 @@ class AuthRepository {
               token: jsonDecode(res.body)['token'],
             );
             error = ErrorModel(error: null, data: newUser);
+            _localStorageRepo.setToken(newUser.token);
+            break;
+        }
+      }
+    } catch (e) {
+      error = ErrorModel(error: e.toString(), data: null);
+    }
+    return error;
+  }
+
+  Future<ErrorModel> getUserData() async {
+    ErrorModel error = ErrorModel(
+      error: 'Something went wrong',
+      data: null,
+    );
+    try {
+      String? token = await _localStorageRepo.getToken();
+
+      if (token != null) {
+        var res = await _client.get(Uri.parse('$host/'), headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+          'x-auth-token': token,
+        });
+        switch (res.statusCode) {
+          case 200:
+            final newUser = UserModel.fromJson(
+              jsonEncode(
+                jsonDecode(res.body)['user'],
+              ),
+            ).copyWith(
+              token: token, 
+            );
+            error = ErrorModel(error: null, data: newUser);
+            _localStorageRepo.setToken(newUser.token);
             break;
         }
       }
--- lib/repository/local_storage_repo.dart
+++ lib/repository/local_storage_repo.dart
@@ -5,4 +5,10 @@ class LocalStorageRepo {
     SharedPreferences preferences = await SharedPreferences.getInstance();
     preferences.setString('x-auth-token', token);
   }
+
+  Future<String?> getToken() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    String? token = preferences.getString('x-auth-token');
+    return token;
+  }
 }
