9b5536bc16719920ae51373f9c3418f334a2eb3c
Teep-m <103185983+Teep-m@users.noreply.github.com>
2022-11-22 23:52:01 +0900

    adding document screen

--- lib/repository/document_repository.dart
+++ lib/repository/document_repository.dart
@@ -1,8 +1,11 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:http/http.dart';
+
 class DocumentRepository {
   final Client _client;
   DocumentRepository({
     required Client client,
-  }): _client = client;
+  }) : _client = client;
 
   Future<ErrorModel> createDocument() async {
     try {
