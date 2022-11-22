b5542822ede3042496a9d8dcb0b26022d4afa105
Teep-m <103185983+Teep-m@users.noreply.github.com>
2022-11-22 23:56:56 +0900

    adding document screen

--- lib/repository/document_repository.dart
+++ lib/repository/document_repository.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:g_docs_clone_flutter/models/error_model.dart';
 import 'package:http/http.dart';
 
 class DocumentRepository {
@@ -8,10 +9,32 @@ class DocumentRepository {
   }) : _client = client;
 
   Future<ErrorModel> createDocument() async {
+    ErrorModel error = ErrorModel(
+      error: 'Something went wrong',
+      data: null,
+    );
     try {
-
+      var res = await _client.get(Uri.parse('$host/'), headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        'x-auth-token': token,
+      });
+      switch (res.statusCode) {
+        case 200:
+          final newUser = UserModel.fromJson(
+            jsonEncode(
+              jsonDecode(res.body)['user'],
+            ),
+          ).copyWith(token: token);
+          error = ErrorModel(error: null, data: newUser);
+          _localStorageRepo.setToken(newUser.token);
+          break;
+      }
     } catch (e) {
-      
+      error = ErrorModel(
+        error: e.toString(),
+        data: null,
+      );
     }
+    return error;
   }
 }
